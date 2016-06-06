bb6067d653db7d904908f2a398152abc6720f1e9
tonussi <lptonussi@gmail.com>
2016-06-06 10:15:28 -0300

    Remove unused itens

--- ic5/main.cpp
+++ ic5/main.cpp
@@ -31,8 +31,9 @@ int main ()
 {
     /* declarações */
     struct sigaction sa;
-
     struct itimerval it_val;
+    struct rusage usage;
+    int who = RUSAGE_SELF;
 
     /**
      * \brief: This structure is used with getrlimit to receive limit values,
@@ -42,8 +43,7 @@ int main ()
      * \param rlim_t rlim_cur: The current limit
      * \param rlim_t rlim_max: The maximum limit.
      */
-    struct rlimit oldLimitTime, newLimitTime;
-    struct rlimit *newProcessorLimitTime;
+    struct rlimit newLimitTime;
 
     int errno;
 
@@ -69,10 +69,10 @@ int main ()
     memset(&sa, 0, sizeof(sa));
     sa.sa_handler = &timer_handler;
     sigaction(SIGVTALRM, &sa, NULL);
-    sigemptyset(&sa.sa_mask);
+    // sigemptyset(&sa.sa_mask);
 
     /* Restart functions if interrupted by handler */
-    sa.sa_flags = SA_RESTART | SA_SIGINFO;
+    // sa.sa_flags = SA_RESTART | SA_SIGINFO;
 
     /**
      * \brief The sigaction() function allows the calling process to examine and/or
@@ -89,7 +89,7 @@ int main ()
     }
 
     /* Configurar o timer para disparar depois de 333ms e a cada 333ms depois disso*/
-    alarm(0.333);
+    // alarm(0.333);
 
     /* Setar o timer para iniciar a contagem enquanto este processo estiver executando */
     it_val.it_value.tv_sec = 0;
@@ -115,22 +115,22 @@ int main ()
      *         EFAULT | O parâmetro new_value, old_value ou curr_value não possui um ponteiro válido
      *         EINVAL | O temporizador informado ou um campo de new_value não é válido
      */
-    if (setitimer(ITIMER_REAL, &it_val, 0) == -1) {
+    if (setitimer(ITIMER_REAL, &it_val, NULL) == -1) {
         printf("Erro setitimer() %d\n", errno);
         exit(1);
     }
 
     /* Obter os limites atuais deste processo */
-    if (prlimit(pid, RLIMIT_CPU, NULL, &oldLimitTime) == -1) {
-        cerr << "prlimit-2" << endl;
-    }
-    printf("New limits: soft=%lld; hard=%lld\n", (long long) oldLimitTime.rlim_cur, (long long) oldLimitTime.rlim_max);
+    // if (prlimit(pid, RLIMIT_CPU, NULL, &oldLimitTime) == -1) {
+    //     cerr << "prlimit-2" << endl;
+    // }
+    // printf("New limits: soft=%lld; hard=%lld\n", (long long) oldLimitTime.rlim_cur, (long long) oldLimitTime.rlim_max);
 
     /* Ajustar o limite de uso da CPU para 2 segundos */
-    newProcessorLimitTime = NULL;
+    // newProcessorLimitTime = NULL;
     //newLimitTime.rlim_cur = 2000;
     newLimitTime.rlim_max = 2;
-    newProcessorLimitTime = &newLimitTime;
+    // newProcessorLimitTime = &newLimitTime;
 
     //if (prlimit(getpid(), RLIMIT_CPU, newProcessorLimitTime, &oldLimitTime) == -1) {
     //    cerr << "prlimit-1" << endl;
@@ -141,12 +141,11 @@ int main ()
 
     /* Ficar preso num laço infinito */
     int i=0;
-    struct rusage usage;
     while (1)
     {
         i = i + 1;
         /* a cada 1 mihão de iterações, obter a utilização atual da CPU */
-        if(i == 10e8) {
+        if(i == 0) {
 
             /**
              *
@@ -157,10 +156,18 @@ int main ()
              * \param ru_stime: A struct timeval field containing the amount of
              * system time, in seconds, that the process has used. System time
              * is the CPU time spent executing system calls on behalf of the process.
+             *
+             * \return:
+             *        On success, zero is returned.  On error, -1 is returned, and errno is
+             *        set appropriately.
+             *
+             * \ERRORS:
+             *        EFAULT usage points outside the accessible address space.
+             *        EINVAL who is invalid.
              */
-            getrusage(RUSAGE_SELF, &usage);
+            getrusage(who, &usage);
             printf ("CPU time: %ld.%06ld sec user, %ld.%06ld sec system\n", usage.ru_utime.tv_sec, usage.ru_utime.tv_usec,  usage.ru_stime.tv_sec, usage.ru_stime.tv_usec);
-            i = 0;
+            i = (i + 1) % 1000000;
         }
     }
     return 0;
